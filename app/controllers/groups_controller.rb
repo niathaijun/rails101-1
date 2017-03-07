4fdf213a91f05e037511be319fdd71de873cf5f9
langweibo <niathaijun2016@gmail.com>
2017-03-07 21:41:54 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,8 +2,20 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
-  
+
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+end
+
+private
+
+def group_params
+  params.require(:group).permit(:title, :description)
+end
+
 end
